7f9f14a0c450c118b48edbdcf41bfc9f70330116
Mateusz Lichwiarz <mateusz.lichwiarz@gmail.com>
2019-11-25 16:37:43 +0100

    Install necessary bundles

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,7 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    FOS\RestBundle\FOSRestBundle::class => ['all' => true],
+    JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
