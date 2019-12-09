94978e68396397632bd9b486228db0e43ba89437
Mateusz Lichwiarz <mateusz.lichwiarz@gmail.com>
2019-12-09 02:05:39 +0100

    Migrate

---
+++ src/Migrations/Version20191208173222.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20191208173222 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE progres ADD date DATE NOT NULL, ADD time TIME NOT NULL, CHANGE weight weight INT DEFAULT NULL, CHANGE sets sets INT DEFAULT NULL, CHANGE reps reps INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE user CHANGE roles roles JSON NOT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE progres DROP date, DROP time, CHANGE weight weight INT DEFAULT NULL, CHANGE sets sets INT DEFAULT NULL, CHANGE reps reps INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE user CHANGE roles roles LONGTEXT CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_bin`');
+    }
+}
