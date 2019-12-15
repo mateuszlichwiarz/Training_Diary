2b973def254b74dc616ece48398c9e736e199d38
Mateusz Lichwiarz <mateusz.lichwiarz@gmail.com>
2019-12-15 02:29:57 +0100

    Create form for update settings

---
+++ src/Form/Type/EditSettingsType.php
@@ -0,0 +1,31 @@
+<?php
+
+    namespace App\Form\Type;
+
+    use App\Entity\User;
+    use App\Entity\HomepageSetting;
+
+    use Symfony\Component\OptionsResolver\OptionsResolver;
+    use Symfony\Component\Form\AbstractType;
+    use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+    use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+    use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+    use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+
+    use Symfony\Component\Form\FormBuilderInterface;
+
+
+    class EditSettingsType extends AbstractType
+    {
+        public function buildForm(FormBuilderInterface $builder, array $options)
+        {
+            $builder
+            ->add('daysEarlier', IntegerType::class, [
+                    'attr' => ['class' => 'form-control'],
+                    'label' => false
+                    ]
+            )
+            ->add('save', SubmitType::class, array('label' => 'Save', 'attr' => array('class' => 'btn btn-success')))
+            ;
+        }
+    }
