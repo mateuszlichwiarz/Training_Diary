18830f262af8c1dcfe58adea1dcc05d2a1b1d373
Mateusz Lichwiarz <mateusz.lichwiarz@gmail.com>
2019-12-15 17:34:03 +0100

    Show workouts improvement for my needs

--- src/Service/ShowWorkouts.php
+++ src/Service/ShowWorkouts.php
@@ -33,6 +33,49 @@
             
         }
 
+        public function getWorkoutsAll($howLong, $user)
+        {
+            $date = new Time();
+            $interestedDate = $date->dateFromAgo($howLong);
+
+            $properties = $this->getPropertiesWorkoutsAll($user, $interestedDate);
+
+            return $properties;
+        }
+        
+
+        private function getPropertiesWorkoutsAll($user, $interestedDate)
+        {
+            $i = 0;
+            $workouts = array();
+
+            $workouts = $this->em
+                ->getRepository(Progres::class)
+                ->findAllWantedWorkouts($interestedDate, $user);
+
+            if($workouts == true){    
+                foreach($workouts as $workout) {
+
+                    $workoutsProperties[$i]['date']     = $workouts[$i]->getDate();
+                    $workoutsProperties[$i]['time']     = $workouts[$i]->getTime();
+                    $workoutsProperties[$i]['day']      = $workouts[$i]->getDay();
+                    $workoutsProperties[$i]['exercise'] = $workouts[$i]->getExercise();
+                    $workoutsProperties[$i]['sets']     = $workouts[$i]->getSets();
+                    $workoutsProperties[$i]['reps']     = $workouts[$i]->getReps();
+                    $workoutsProperties[$i]['weight']   = $workouts[$i]->getWeight();
+
+                    $i++;
+                }
+
+
+                return $workoutsProperties;
+
+            }else {
+
+                return 0;
+            }
+        }
+
         private function getPropertiesWorkouts($user, $interestedDate, $currentdate)
         {
 
